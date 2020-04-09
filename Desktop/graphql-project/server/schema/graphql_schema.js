0f83cabb59f22951185a40083dcb3f320f3e307f
Somnathpaul <somnath4x@gmail.com>
2020-04-09 18:37:47 +0530

    Modified

--- Desktop/graphql-project/server/schema/graphql_schema.js
+++ Desktop/graphql-project/server/schema/graphql_schema.js
@@ -1,12 +1,29 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString, GraphQLInt} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: ()=>({
-    id: {type: GraphQLInt},
+    id: {type: GraphQLString},
     name: {type: GraphQLString},
     genre: {type: GraphQLString}
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: {type:GraphQLString}},
+      resolve(parent, args){
+        // code to get data from db
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
