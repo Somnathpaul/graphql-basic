ff2f2bd42437bbc23b82d96da00c0be325fed684
Somnathpaul <somnath4x@gmail.com>
2020-04-15 16:31:10 +0530

    Add: mongodb atlas connection

--- Desktop/graphql-project/server/app.js
+++ Desktop/graphql-project/server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema  =  require('./schema/graphql_schema');
+const connectDB = require('../config/db');
 
 const app =  express();
+
+// connect to mongodb
+connectDB();
+
 app.use('/graphql', graphqlHTTP({
   schema,
   // to add the gui testing 
